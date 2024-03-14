3f81d92c0c56edfba58090279a24a1ebe735e941
yagoa <yagoa@SantiagoLaptop>
2024-03-13 20:52:55 -0600

    Primer codigo

---
+++ src/main/java/com/mycompany/iniciosecion/InicioSecion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.iniciosecion;
+
+/**
+ *
+ * @author yagoa
+ */
+public class InicioSecion {
+
+    public static void main(String[] args) {
+        
+    }
+}
